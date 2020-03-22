9451a0e91de5248b2bce1626bab8c9189a3a72d4
Teddy Wing <git@teddywing.com>
2020-03-22 15:04:43 +0100

    Add link to source code on pkg.go.dev
    Link to the package's source code on pkg.go.dev's documentation page.

--- godoc-links.user.js
+++ godoc-links.user.js
@@ -1,1 +1,14 @@
 document.body.innerHTML = document.body.innerHTML.replace(/import "([^"]+)"/, 'import "<a href="https://$1">$1</a>"')
+
+
+function pkg_go_dev () {
+	var import_path_el = document.querySelector('.DetailsHeader-breadcrumbCurrent');
+
+	var link_el = document.createElement('a');
+	link_el.textContent = import_path_el.textContent;
+	link_el.href = 'https://' + import_path_el.textContent;
+
+	var parent_el = import_path_el.parentNode;
+	parent_el.removeChild(import_path_el);
+	parent_el.prepend(link_el);
+}
